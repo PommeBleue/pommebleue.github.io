4b7c77911f25883657f6fb736de9d0c504cb5f7a
Amar Ahmane <tiplouf@machine.local>
2024-12-12 00:54:29 +0100

    Compiling tikz to svg

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if [[ $1 == "svg" ]]; then 
-    xelatex -output-directory="./src/latex/${2}/${3}" "./src/latex/${2}/${3}/${3}.tex"
-elif [[ $1 == "classic" ]]; then 
-    xelatex -output-directory="./src/latex/${2}/${3}" "./src/latex/${2}/${3}/${3}.tex"
+if [[ $1 == "classic" ]]; then 
+    pdflatex -output-directory="./src/latex/${2}/${3}" "./src/latex/${2}/${3}/${3}.tex"
+elif [[ $1 == "svg" ]]; then 
+    lualatex --output-format=dvi -output-directory="./src/latex/${2}/${3}" "./src/latex/${2}/${3}/${3}"
+    dvisvgm --output-directory="./src/latex/${2}/${3}" ./src/latex/${2}/${3}/${3}
 else
     echo "Argument specified not recognized"
 fi
