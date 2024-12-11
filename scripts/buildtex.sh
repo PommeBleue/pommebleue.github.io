2fce2f2926d8e6d9470764868df15da09440d59e
Amar Ahmane <tiplouf@machine.local>
2024-12-11 17:40:41 +0100

    Deleting some files

--- scripts/buildtex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pdflatex -synctex=1 -interaction=nonstopmode -output-directory='./src/tex/colles/' './src/tex/colles/main.tex'
-
-rm -vf ./src/tex/*/*.log ./src/tex/*/*.aux ./src/tex/*/*.idx ./src/tex/*/*.toc ./src/tex/*/*.synctex.gz ./src/tex/*/*.out ./src/tex/*/*.dvi ./src/tex/*/*.fdb_latexmk ./src/tex/*/*.fls ./src/tex/*/*.ilg ./src/tex/*/*.ind ./src/tex/*/*.nlo ./src/tex/*/*.nls
