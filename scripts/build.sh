149691b4ed2659ebfd04d7ab04cc6c2bdbfec08a
Amar Ahmane <tiplouf@machine.local>
2024-12-29 00:30:20 +0100

    Somethiings

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+update_footer_date() {
+    for file in $(git ls-files '*.html'); do
+        last_commit_date=$(git log -1 --format="%ad" --date=short -- "$file")
+        sed -i '' "s/Updated on .*/Updated on $last_commit_date<\/p>/" "$file"
+    done
+}
 
-if [[ $1 == "classic" ]]; then 
+if [[ $1 == "tex-classic" ]]; then 
     xelatex -output-directory="./src/latex/${2}/${3}" "./src/latex/${2}/${3}/${3}.tex"
-elif [[ $1 == "svg" ]]; then 
+elif [[ $1 == "tex-svg" ]]; then 
     xelatex -output-directory="./src/latex/${2}/${3}" "./src/latex/${2}/${3}/${3}.tex"
     pdf2svg "./src/latex/${2}/${3}/${3}.pdf" "./src/latex/${2}/${3}/${3}.svg"
-else
-    echo "Argument specified not recognized"
+elif [[ $1 == "commit" ]]; then
+    git add . 
+    git commit -m $2
+    update_footer_date
+    git push origin main
 fi
-
